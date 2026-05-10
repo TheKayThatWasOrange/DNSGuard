2eea4df67c111dadcb4b8ed6d63d3f7616672859
Orange Kay <github.cb8b5d@maildepot.net>
2026-05-10 02:43:53 -0700

    Order preservation

--- src/dnsguard/dnsguard.py
+++ src/dnsguard/dnsguard.py
@@ -1,15 +1,54 @@
+import typer
+import re
 import SystemConfiguration
 import dispatch
+from ordered_set import OrderedSet
 from PyObjCTools import AppHelper
+from rich import print
+
+IS_IP_ADDRESS = re.compile(r"^(((?!25?[6-9])[12]\d|[1-9])?\d\.?\b){4}$", re.IGNORECASE)
 
 
-def main():
+def main(preferred_servers: list[str]):
+
+    for server in preferred_servers:
+        if not IS_IP_ADDRESS.match(server):
+            print(f"'{server}' is not an IPv4 address.")
+            raise typer.Abort()
+
+    valid_servers = OrderedSet(preferred_servers)
+
+    def config_callback(store, keys, context=None):
+        for key in keys:
+            value = SystemConfiguration.SCDynamicStoreCopyValue(store, key)
+
+            try:
+                stored_servers = value.get("ServerAddresses", None)
+
+                if stored_servers is None:
+                    stored_servers = [value.get("ServerAddress", None)]
+
+                if set(stored_servers) != valid_servers:
+                    print(f"[red]{key}: {stored_servers} != {valid_servers}[/red]")
+                else:
+                    print(f"[green]{key} is in compliance.[/green]")
+            except Exception:
+                print(
+                    f"[yellow]{key} cannot be verified and is probably irrelevant.[/yellow]"
+                )
+                pass
+
     store = SystemConfiguration.SCDynamicStoreCreate(
         None, "DNSGuard", config_callback, None
     )
 
     dns_related_keys = SystemConfiguration.SCDynamicStoreCopyKeyList(store, ".*DNS")
 
+    # Manually fire the callback once to pick up any non-compliance
+    # without waiting around for something to change.
+    config_callback(store, dns_related_keys)
+
+    # Then start watching those same keys for future changes.
     if SystemConfiguration.SCDynamicStoreSetNotificationKeys(
         store, dns_related_keys, None
     ):
@@ -19,20 +58,10 @@ def main():
         dispatch_queue = dispatch.dispatch_get_main_queue()
 
         if SystemConfiguration.SCDynamicStoreSetDispatchQueue(store, dispatch_queue):
-            print("Now Watching: ")
-            print(dns_related_keys)
-            print(store)
-
-            # dispatch.dispatch_main()
+            print("Watching the store for changes...")
+            # This handles keyboard input. dispatch_main() does not.
             AppHelper.runConsoleEventLoop(installInterrupt=True)
 
 
-def config_callback(store, keys, context):
-    print(f"Some shit changed in {keys}:")
-    for key in keys:
-        value = SystemConfiguration.SCDynamicStoreCopyValue(store, key)
-        print(value)
-
-
 if __name__ == "__main__":
-    main()
+    typer.run(main)
